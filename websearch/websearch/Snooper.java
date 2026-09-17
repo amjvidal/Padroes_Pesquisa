ea12142ca2f3cd73744ec7ff40a7325ddc967c55
Lucas Teixeira <tlucasmatheus@gmail.com>
2026-09-17 20:32:50 -0300

    feat: Criar observadores no Snooper usando filtros específicos

--- websearch/websearch/Snooper.java
+++ websearch/websearch/Snooper.java
@@ -2,16 +2,38 @@
  * Watches the search queries
  */
 public class Snooper {
-    private final WebSearchModel model;
+    public final WebSearchModel model;
 
     public Snooper(WebSearchModel model) {
         this.model = model;
 
-        model.addQueryObserver( new WebSearchModel.QueryObserver() {
-            @Override
-            public void onQuery(String query) {
-                System.out.println("Query: " + query);
-            }
-        });
+        model.addQueryObserver(
+                new WebSearchModel.QueryObserver() {
+                    @Override
+                    public void onQuery(String query) {
+                        System.out.println("Oh Yes! " + query);
+                    }
+                },
+                new QueryFilter() {
+                    @Override
+                    public boolean accepts(String query) {
+                        return query.toLowerCase().contains("friend");
+                    }
+                });
+
+        model.addQueryObserver(
+                new WebSearchModel.QueryObserver() {
+                    @Override
+                    public void onQuery(String query) {
+                        System.out.println("So long........ " + query);
+                    }
+                },
+
+                new QueryFilter() {
+                    @Override
+                    public boolean accepts(String query) {
+                        return query.length() > 60;
+                    }
+                });
     }
 }
